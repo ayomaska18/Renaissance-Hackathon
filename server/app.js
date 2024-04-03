7a78c9c0603dd9d99e323513867466ecf455ae8f
ayomaska18 <MaskaChung123@gmail.com>
2024-04-03 19:50:04 +0100

    removed comments on app.js

--- server/app.js
+++ server/app.js
@@ -18,59 +18,59 @@ let solTestNetAddress = 'HpWC6RNZYa9Q5E5fqBoeAfZvGHMMw1xLN4izUKErSsTh';
 let url = 'https://www.facebook.com/';
 
 async function init() {
-  // const timeoutPromise = new Promise((_, reject) => {
-  //   setTimeout(() => reject(new Error('Operation timed out.')), 180000);
-  // });
+  const timeoutPromise = new Promise((_, reject) => {
+    setTimeout(() => reject(new Error('Operation timed out.')), 180000);
+  });
 
-  // try {
-  //   const initLogicPromise = (async () => {
-  //   const builtWtihData = await builtWithService.getBuiltWithInfo(builtWithApi, url);
-  //   const techSpendData = builtWithService.getSpendOnTech(builtWtihData);
+  try {
+    const initLogicPromise = (async () => {
+    const builtWtihData = await builtWithService.getBuiltWithInfo(builtWithApi, url);
+    const techSpendData = builtWithService.getSpendOnTech(builtWtihData);
 
-  //   const whoIsData = await whoIsService.getWhoIsData(whoIsApi, url);
-  //   const webCreationDateData = whoIsService.getCreationDate(whoIsData);
-  //   const companyLocation = whoIsService.getCompanyLocation(whoIsData);
+    const whoIsData = await whoIsService.getWhoIsData(whoIsApi, url);
+    const webCreationDateData = whoIsService.getCreationDate(whoIsData);
+    const companyLocation = whoIsService.getCompanyLocation(whoIsData);
 
-  //   const sslData = await sslLabsService.analyzeWithSSLabs(url);
-  //   const sslTrustGrade = sslLabsService.getTrustGrade(sslData);
+    const sslData = await sslLabsService.analyzeWithSSLabs(url);
+    const sslTrustGrade = sslLabsService.getTrustGrade(sslData);
 
-  //   const whoHostdata = await whoHostService.getWhoHostThisData( whoHostApi, url)
-  //   const webHostingData = whoHostService.getWebHosting(whoHostdata);
+    const whoHostdata = await whoHostService.getWhoHostThisData( whoHostApi, url)
+    const webHostingData = whoHostService.getWebHosting(whoHostdata);
 
-  //   console.log("-----------------")
-  //   console.log("Project Analysis");
-  //   console.log("-----------------")
-  //   console.log("Web Analysis:", url);
-  //   console.log("Tech Investments Monthly:", techSpendData);
-  //   console.log("Web Creation Date:", webCreationDateData);
-  //   console.log("Company Location:", companyLocation);
-  //   console.log("SSL labs Trust Grade:", sslTrustGrade);
-  //   console.log("Web Hosting Provider:", webHostingData);
-  //   console.log("-----------------")
-  //   console.log("Wallet Analysis");
-  //   console.log("-----------------")
-  //   console.log('Address:', solTestNetAddress);
+    console.log("-----------------")
+    console.log("Project Analysis");
+    console.log("-----------------")
+    console.log("Web Analysis:", url);
+    console.log("Tech Investments Monthly:", techSpendData);
+    console.log("Web Creation Date:", webCreationDateData);
+    console.log("Company Location:", companyLocation);
+    console.log("SSL labs Trust Grade:", sslTrustGrade);
+    console.log("Web Hosting Provider:", webHostingData);
+    console.log("-----------------")
+    console.log("Wallet Analysis");
+    console.log("-----------------")
+    console.log('Address:', solTestNetAddress);
 
-  //   analyzeWalletVolume.getBlockTimes(solTestNetAddress)
-  //   .then(async blockTimes => {
-  //       const counts = analyzeWalletVolume.countDatesForMostRecentMonth(blockTimes);
-  //       const activityRating = analyzeWalletVolume.checkRecentMonthWalletActivity(counts);
-  //       console.log("Wallet Activity Rating:", activityRating);
-  //   })
-  //   .catch(err => console.error(err));
+    analyzeWalletVolume.getBlockTimes(solTestNetAddress)
+    .then(async blockTimes => {
+        const counts = analyzeWalletVolume.countDatesForMostRecentMonth(blockTimes);
+        const activityRating = analyzeWalletVolume.checkRecentMonthWalletActivity(counts);
+        console.log("Wallet Activity Rating:", activityRating);
+    })
+    .catch(err => console.error(err));
 
-  //   analyzeWalletBalance.getTokenBalance(solTestNetAddress)
-  //   .then(walletBalance => {
-  //   })
-  //   .catch(err => console.error(err));
-  // })();
+    analyzeWalletBalance.getTokenBalance(solTestNetAddress)
+    .then(walletBalance => {
+    })
+    .catch(err => console.error(err));
+  })();
 
-  //   await Promise.race([initLogicPromise, timeoutPromise]);
+    await Promise.race([initLogicPromise, timeoutPromise]);
 
 
-  // } catch (error) {
-  //   console.error('Failed to fetch data:', error);
-  // }
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
   (async () => {
     try {
         await checkStakingActivities.getStakingActivities('HpWC6RNZYa9Q5E5fqBoeAfZvGHMMw1xLN4izUKErSsTh');
